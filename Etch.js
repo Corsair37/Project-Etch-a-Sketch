ab2ae31409602583ffd92ef78f648364723539b5
Corsair <999.ala37@gmail.com>
2024-03-27 09:54:17 -0400

    Add pixel trail
    1st attempt to add a pixel trail effect by creating small "pixel" divs on the grid when the mouse hovers over.

--- Etch.js
+++ Etch.js
@@ -11,3 +11,27 @@ function fillGrid () {
 }
 
 let filledGrid = fillGrid();
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let container = document.getElementById("grid");
+
+    container.addEventListener("mousemove", function(event) {
+        createPixel(event.pageX, event.pageY);
+    });
+
+    function createPixel(x, y) {
+        let pixel = document.createElement("div");
+        pixel.className = "pixel";
+        pixel.style.left = x + "px";
+        pixel.style.top = y + "px";
+        container.appendChild(pixel);
+
+        setTimeout(function() {
+            pixel.style.opacity = 0;
+            setTimeout(function() {
+                pixel.remove();
+            }, 300);
+        }, 1000);
+    }
+});
