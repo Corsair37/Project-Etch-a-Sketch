81e20e4a357ac051560fb0ed1563e9890907a26b
Corsair <999.ala37@gmail.com>
2024-03-27 08:13:44 -0400

    Fix fillGrid function
    The function had previously attempted to return an empty variable. Now, it returns the new altered old variable instead of the empty new one. The "empty" variable filledGrid is now outside of the function and acts as a reference to the function.

--- Etch.js
+++ Etch.js
@@ -1,11 +1,12 @@
-const grid = document.getElementById ("grid");
+let grid = document.getElementById ("grid");
 
 function fillGrid () {
-    let filledGrid
     for (let i = 0; i < 256 ; i++) {
         let newDiv = document.createElement ("div");
         grid.appendChild (newDiv)
-        return filledGrid
+        return grid
     }
 
 }
+
+let filledGrid = fillGrid();
