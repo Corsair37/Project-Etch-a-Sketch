dd692bd6a201e69f9a8cee11043a9d375d5c8eb0
Corsair <999.ala37@gmail.com>
2024-03-27 08:04:31 -0400

    Create and fill outer grid
    In order to create a 16 x 16 grid, an outer div (id = "grid") has been created. Then, applying flexbox attributes to the grid allows it to stretch. Finally, using JS to develop a function that creates 256 inner boxes, then returns the filled grid as a single variable.

---
+++ Etch.js
@@ -0,0 +1,11 @@
+const grid = document.getElementById ("grid");
+
+function fillGrid () {
+    let filledGrid
+    for (let i = 0; i < 256 ; i++) {
+        let newDiv = document.createElement ("div");
+        grid.appendChild (newDiv)
+        return filledGrid
+    }
+
+}
