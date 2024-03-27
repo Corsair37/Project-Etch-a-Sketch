6330a93c6e6bc0c830a98cfc6aff7e75b86ef6f0
Corsair <999.ala37@gmail.com>
2024-03-27 08:21:27 -0400

    Add header and style boxes
    The inner boxes of the grid are now styled in order to gather where they are on the page as of now. A header "Etch-a-Sketch" has been inserted, and the JS now adds classes to these created boxes.

--- Etch.js
+++ Etch.js
@@ -3,6 +3,8 @@ let grid = document.getElementById ("grid");
 function fillGrid () {
     for (let i = 0; i < 256 ; i++) {
         let newDiv = document.createElement ("div");
+        newDiv.className = "gridBox";
+        newDiv.textContent= "16"
         grid.appendChild (newDiv)
         return grid
     }
